241807a68024df53ec6e5aadcea621b8639b6e79
HSH <han41562@gmail.com>
2024-03-21 11:34:32 +0900

    feat: add HttpRequest util method

--- src/main/java/com/example/request/HttpRequest.java
+++ src/main/java/com/example/request/HttpRequest.java
@@ -28,7 +28,7 @@ public class HttpRequest {
         setMethod(startLine[0]);
 
         String[] str = startLine[1].split("\\?");
-        if(str.length >= 0 || str.length >= 3)
+        if(str.length <= 0 || str.length >= 3)
             throw new IllegalArgumentException("Invalid Http Request");
         setPath(str[0]);
 
@@ -55,10 +55,38 @@ public class HttpRequest {
     }
 
     public void addHeader(HttpRequestUtils.Pair pair){
-        headers.put(pair.getKey(), pair.getValue());
+        headers.put(pair.getKey().toLowerCase(), pair.getValue());
+    }
+
+    public boolean containsHeader(String key){
+        return headers.containsKey(key);
+    }
+
+    public String findHeader(String key){
+        return headers.get(key);
     }
 
     public void setMessageBody(String messageBody) {
         this.messageBody = messageBody;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String startLine = String.join(" ", method.toString(),
+                path, version);
+        sb.append(startLine);
+        sb.append("\r\n");
+        for(Map.Entry e : headers.entrySet()){
+            sb.append(e.getKey() + ": "+e.getValue());
+            sb.append("\r\n");
+        }
+        sb.append("\r\n");
+
+        if(messageBody != null && !messageBody.isBlank()){
+            sb.append(messageBody);
+        }
+
+        return sb.toString();
     }
 }
